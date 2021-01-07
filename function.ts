f7183ad742045ebd580adf2d20737e4a3a0f0b54
Rthem@gmx.de <ma51adof>
2021-01-07 17:47:06 +0100

    Initial commit

---
+++ function.ts
@@ -0,0 +1,13 @@
+var msg:string = 'test'
+console.log(msg)
+
+function fkt (zahl:number):Boolean{
+    return zahl > 42
+}
+
+function fv (zahl:number):void{
+    console.log(zahl)
+}
+
+// null         Hat Wert, Wert ist Null
+// undefined    Es gibt kein Wert
