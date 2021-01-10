997c01d78e5931745cf37f77f61ba372240bd902
Rthem@gmx.de <ma51adof>
2021-01-10 19:13:38 +0100

    tut 5

--- function.ts
+++ function.ts
@@ -1,6 +1,3 @@
-var msg:string = 'test'
-console.log(msg)
-
 function fkt (zahl:number):Boolean{
     return zahl > 42
 }
@@ -9,5 +6,17 @@ function fv (zahl:number):void{
     console.log(zahl)
 }
 
-// null         Hat Wert, Wert ist Null
-// undefined    Es gibt kein Wert
+// null                 Hat Wert, Wert ist Null
+// undefined            Es gibt kein Wert
+var mss:string = 'hello string'
+var mgg:any = null          //any ist standard, configured auto
+
+var luise:number[] = [43,563,6734]
+console.log(luise[2])
+
+var uno:number|string = 42
+console.log(typeof uno)
+uno = "hello union"
+console.log(typeof uno)
+// uno = true               erzeugt Fehler
+
