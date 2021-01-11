1359fbf1983d90a8598fb0db7e0934a732df8417
Rthem@gmx.de <ma51adof>
2021-01-11 21:11:58 +0100

    tut 6

--- function.ts
+++ function.ts
@@ -20,3 +20,42 @@ uno = "hello union"
 console.log(typeof uno)
 // uno = true               erzeugt Fehler
 
+class apartment{
+    width:number
+    height:number
+    rooms:number
+    balkon:boolean
+
+    constructor(z:number) {     // = sehr spezielle Methode, die wenn das Objekt erstellt wird greift
+    this.rooms = z              // Das muss nun eingegeben werden
+    }
+
+
+    methoge():number{
+        if (this.height && this.width){
+            return this.width + this.height
+        } //wird je Instanz berechnet
+        else {
+            return 0
+        }
+    }
+}
+var my_apartment:apartment = new apartment(3)
+console.log(my_apartment.balkon)
+my_apartment.balkon = true
+my_apartment.rooms = 3              //Fällt durch constructor weg
+my_apartment.width = 300
+my_apartment.height = 200
+console.log(my_apartment.methoge())
+
+var my_house:apartment = new apartment(2)
+my_apartment.width = 300
+console.log(my_house.methoge())
+
+
+
+
+
+
+
+
