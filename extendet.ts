eadc3b64bee9f9cf9f3cb8eb5a71b124da1e9718
Rthem@gmx.de <ma51adof>
2021-01-14 15:58:23 +0100

    tut 9

--- extendet.ts
+++ extendet.ts
@@ -23,6 +23,21 @@ bonus.tun() // Hierarchie wird von unten begonnen
 
 /*
 Interface = Oberklasse, die man nicht verwenden kann, weil sie durch eine Unterklasse erst genauer spezifiziert werden soll.
+Vor jedes Interface schreibt man I, eine Klasse kann mehrere Interfaces Implementieren.
  */
+interface ISprache{
+    //Veränderte Methode
+    Satz:(length:number) => void
+}
+interface Wort extends ISprache{             //Das extends verwendet wird ist hier merkwürdig.
+    Buchstabe:(character:string) => void
+}
+class Buchstabe implements Wort{
+    Buchstabe(character: string): void {
+        // TODO rumlachsen
+    }
 
-
+    Satz(length: number): void {
+        //TODO envi defi
+    }
+}
